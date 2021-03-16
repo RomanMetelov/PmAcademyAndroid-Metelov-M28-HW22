351e09205e44f4661318f54b3aa55d91fa584ee1
Roman Metelov <rm0734861369@gmail.com>
2021-03-17 01:54:10 +0200

    detekt added

--- app/src/main/java/com/example/pmacademyandroid_metelov_m28_hw22/App.kt
+++ app/src/main/java/com/example/pmacademyandroid_metelov_m28_hw22/App.kt
@@ -15,6 +15,8 @@ class App : Application() {
             .roomModule(RoomModule(this))
             .dispatcherModule(DispatcherModule)
             .build()
+
+
     }
 
     fun getComponent(): AppComponent {
